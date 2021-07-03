a3bec0d428cd61ca0f817d3f54e85e9a2a8488b4
Joe <joe.syx@gmail.com>
2021-07-03 12:17:49 +1000

    Feature - add a few feature methods

--- src/Observer.php
+++ src/Observer.php
@@ -7,4 +7,59 @@ class Observer
     {
 
     }
+
+    public function hello()
+    {
+
+    }
+
+    public function world()
+    {
+        
+    }
+
+    public function masterOne()
+    {
+
+    }
+
+    public function masterTwo()
+    {
+        
+    }
+
+    public function masterThree()
+    {
+
+    }
+
+    public function masterFour()
+    {
+        
+    }
+
+    public function one()
+    {
+        // TODO:: one
+    }
+
+    public function two()
+    {
+        // TODO:: two
+    }
+
+    public function three()
+    {
+
+    }
+
+    public function four()
+    {
+        
+    }
+
+    public function five()
+    {
+        
+    }
 }
